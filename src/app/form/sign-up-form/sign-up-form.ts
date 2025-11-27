d5eb0068905779433bfdebf4e3e0548a4749bd95
arpco13 <arpcothirteen@outlook.com>
2025-11-27 11:27:37 +0330

    updated sign upto reactive forms

--- src/app/form/sign-up-form/sign-up-form.ts
+++ src/app/form/sign-up-form/sign-up-form.ts
@@ -1,22 +1,52 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component, signal, computed, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { TextField } from '../../text-field/text-field';
 import { PassValidationService } from '../../../services/pass-validation/pass-validation';
 import { UserNameValidationService } from '../../../services/user-name-validation/user-name-validation';
 
 @Component({
   selector: 'app-sign-up-form',
-  imports: [TextField],
+  imports: [TextField, ReactiveFormsModule],
   templateUrl: './sign-up-form.html',
 })
-export class SignUpForm {
-  password = signal('');
-  username = signal('');
+export class SignUpForm implements OnInit {
+  signUpForm!: FormGroup;
+
+  // Create signals for form values to trigger computed updates
+  formValues = signal({
+    username: '',
+    password: '',
+  });
+
   constructor(
+    private fb: FormBuilder,
     private passvalidation: PassValidationService,
     private usernvalidation: UserNameValidationService
   ) {}
-  passwordValidation = computed(() => this.passvalidation.validatePassword(this.password()));
-  usernameValidation = computed(() => this.usernvalidation.validateUsername(this.username()));
+
+  ngOnInit() {
+    this.signUpForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+
+    // Listen to form value changes and update the signal
+    this.signUpForm.valueChanges.subscribe((values) => {
+      this.formValues.set({
+        username: values.username || '',
+        password: values.password || '',
+      });
+    });
+  }
+
+  // Update computed properties to use the formValues signal
+  passwordValidation = computed(() =>
+    this.passvalidation.validatePassword(this.formValues().password)
+  );
+
+  usernameValidation = computed(() =>
+    this.usernvalidation.validateUsername(this.formValues().username)
+  );
 
   passwordScore = computed(() => this.passwordValidation().extra.score);
   passwordColor = computed(() => this.passwordValidation().extra.color);
